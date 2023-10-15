c0f3aa4cdf24c687e049f7ac230e96d91866e126
Borys-Grinberg <borys.grinberg@gmail.com>
2023-10-15 20:00:40 +0200

    task1 edit

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
-  const categoryElements = category.querySelectorAll("ul li");
+  const categoryElements = category.querySelector("ul").children;
 
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements.length}`);
